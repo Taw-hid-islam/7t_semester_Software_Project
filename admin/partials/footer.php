512e48315e3a1af4055952f2879ff163527bbdc1
Taw-hid-islam <tawhidul5790@gmail.com>
2024-12-02 20:22:54 +0600

    updated

---
+++ admin/partials/footer.php
@@ -0,0 +1,29 @@
+<!-- footer section start-->
+<div class="footer">
+            <div class="wrapper">
+           <p class="text-center" >2024 all right reserved by Trio's Restro. Developed by <a href=#> skadush </a></P>
+           </div>
+          
+        </div>
+        <!--footer section ends-->
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </body>
+
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,45 @@
+<?php include("../config/constant.php"); ?>
+
+ 
+
+
+<html>
+    <head>
+        <title>Food order website-home page</title>
+        <link rel="stylesheet" href="../css/addi.css">
+         <!-- Latest compiled and minified CSS -->
+         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <!-- Menu section start-->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="manage_admin.php">Admin</a></li>
+                    <li><a href="order.php">Order</a></li>
+                    <li><a href="manage_catagory.php">Catagory</a></li>
+                    <li><a href="manage_food.php">Food</a></li>
+                    <li><a href="booking.php">Booking</a></li>
+                    <li><a href="user.php">User</a></li>
+                    <li><a href="supplier.php">Supplier</a></li>
+                    <li><a href="staff.php">Staff</a></li>
+                    <div class="logout">
+                        <a href="../logout.php" class="btn btn-danger">Logout</a>
+                    </div>
+                    
+
+
+                </ul>
+            
+            </div>
+           
+        </div>
+        <!--menu section ends-->
